c654a46ca3c9416dcf35c414e4fc82f3f4dc2572
Lucas F Marçoni Ferreira <lucasfmarconi@gmail.com>
2025-03-12 07:36:25 -0300

    Implement machine control subscription

--- main.py
+++ main.py
@@ -9,44 +9,77 @@ import ssl
 # region Initialisation
 led = machine.Pin(2, machine.Pin.OUT)
 dht_pin = dht.DHT11(machine.Pin(17))
+LOOP_TIME = config.LOOP_TIME
+DHT11_WAIT_TIME = config.DHT11_WAIT_TIME
 # endregion
 
 def blink_led():
     print("Blinking LED")
     led.value(1)  # Turn the LED on
-    time.sleep(5)  # Wait for 5 seconds
+    time.sleep(5)
     led.value(0)  # Turn the LED off
-    time.sleep(1)  # Wait for 1 second
 
 # region DHT11
 def read_dht11():
     print("Measuring T and H with DHT11")
-    time.sleep(15)  # Initial delay to ensure the sensor is ready
+    time.sleep(DHT11_WAIT_TIME)  # Initial delay to ensure the sensor is ready
     try:
         dht_pin.measure()
         temperature = dht_pin.temperature()
         humidity = dht_pin.humidity()
 
-        temperature_topic = config.MQTT_TOPIC_PUB + "/temperature"
-        print("Publishing to topic:", temperature_topic)
-        mqtt_client.publish(temperature_topic, bytes(str(temperature), "utf-8"))
-
-        humidity_topic = config.MQTT_TOPIC_PUB + "/humidity"
-        print("Publishing to topic:", humidity_topic)
-        mqtt_client.publish(humidity_topic, bytes(str(humidity), "utf-8"))
+        publish_temperature(temperature)
+        publish_humidity(humidity)
 
         print("Temperature: {}°C, Humidity: {}%".format(temperature, humidity))
     except OSError as e:
-        print("Failed to read sensor:", e)
+        print("Failed to read DHT sensor:", e)
+
 # endregion
 
 
 # region MQTT
-def sub_cb(topic, msg):
-    print((topic, msg))
+def publish_humidity(humidity):
+    humidity_topic = config.MQTT_TOPIC_PUB + "/humidity"
+    print("Publishing to topic:", humidity_topic)
+    mqtt_client.publish(humidity_topic, bytes(str(humidity), "utf-8"))
 
 
-def connect_and_subscribe():
+def publish_temperature(temperature):
+    temperature_topic = config.MQTT_TOPIC_PUB + "/temperature"
+    print("Publishing to topic:", temperature_topic)
+    mqtt_client.publish(temperature_topic, bytes(str(temperature), "utf-8"))
+
+
+def process_control_message(topic, msg):
+    print("Received message:", (topic, msg))
+    if "restart" in msg:
+        restart()
+    elif "blink" in msg:
+        blink_led()
+    elif "airsensor-read" in msg:
+        read_dht11()
+    elif "change-loop-time" in msg:
+        update_loop_time(msg)
+    elif "change-dht-read-time" in msg:
+        update_dht_read_time(msg)
+    else:
+        print("Unknown message on topic {} msg: {}".format(topic, msg))
+
+
+def update_loop_time(msg):
+    global LOOP_TIME
+    LOOP_TIME = int(msg)
+    print("Changed loop time to:", config.LOOP_TIME)
+
+
+def update_dht_read_time(msg):
+    global DHT11_WAIT_TIME
+    DHT11_WAIT_TIME = int(msg)
+    print("Changed loop time to:", config.DHT11_WAIT_TIME)
+
+
+def connect_to_mqtt_broker():
     ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_CLIENT)
     ssl_context.verify_mode = ssl.CERT_NONE
 
@@ -57,37 +90,45 @@ def connect_and_subscribe():
         password=config.MQTT_PASS,
         ssl=ssl_context,
     )
-    mqtt_client.set_callback(sub_cb)
-    mqtt_client.connect()
-    mqtt_client.subscribe(config.MQTT_TOPIC_SUB)
-    print(
-        "Connected to %s MQTT broker, subscribed to %s topic"
-        % (config.MQTT_SERVER, config.MQTT_TOPIC_SUB)
+
+    mqtt_client.set_last_will(
+        "lwt/machines/mpy-esp32", "Machine is offline", retain=True
     )
+
+    mqtt_client.connect()
     return mqtt_client
 
 
-def restart_and_reconnect():
-    print("Failed to connect to MQTT broker. Reconnecting...")
+def restart():
     time.sleep(10)
+    print("Restarting machine...")
     machine.reset()
+
+
+def subscribe_to_machine_control_topic():
+    print("Subscribing to machine control topic:", config.MQTT_TOPIC_SUB)
+    mqtt_client.set_callback(process_control_message)
+    mqtt_client.subscribe(config.MQTT_TOPIC_SUB)
+
 
 try:
     print("\nStarting MQTT Client...")
-    mqtt_client = connect_and_subscribe()
+    mqtt_client = connect_to_mqtt_broker()
+    subscribe_to_machine_control_topic()
 except OSError as e:
-    restart_and_reconnect()
+    print("Failed to connect to MQTT broker")
+    restart()
 
 # endregion
 
 while True:
     print("\nMain loop started...")
     try:
-        new_message = mqtt_client.check_msg()
-        if new_message is not None:
-            print("Received message:", new_message)
+        received_message = mqtt_client.check_msg()
+        if received_message is not None:
+            print("Received pending message during startup:", received_message)
         blink_led()
         read_dht11()
-        time.sleep(1)  # Wait for X seconds before the next loop iteration
+        time.sleep(config.LOOP_TIME)
     except OSError as e:
-        restart_and_reconnect()
+        restart()
